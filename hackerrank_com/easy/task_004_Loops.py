366627b23f68901978fc66c3622fdc0545287a18
SKO7OPENDRA <58439768+SKO7OPENDRA@users.noreply.github.com>
2020-02-25 11:27:16 +0300

    a (// /) b

--- hackerrank_com/easy/task_004_Loops.py
+++ hackerrank_com/easy/task_004_Loops.py
@@ -1,4 +1,11 @@
-# Read an integer . For all non-negative integers i<N, print i**2 . See the sample for details.
+# Read an integer N. For all non-negative integers i < N, print i**2 . See the sample for details.
+
+# Input Format
+# The first and only line contains the integer, N.
+# Constraints
+# 1 <= N <= 20
+# Output Format
+# Print  lines, one corresponding to each .
 
 N = int(input())
 i = 0
